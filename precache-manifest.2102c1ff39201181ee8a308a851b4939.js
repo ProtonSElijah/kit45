5f993b8d60c0d2cff3e13beab5fb56149af20ad6
Georgii Timofeevskii <gtimoshaz@gmail.com>
2019-12-05 13:24:57 +0300

    Updates

---
+++ precache-manifest.2102c1ff39201181ee8a308a851b4939.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25bfc6841fd323e38dd78701c8e9745",
+    "url": "/kit45/index.html"
+  },
+  {
+    "revision": "26d6bdea9cce883bf0a2",
+    "url": "/kit45/static/css/main.49e1724e.chunk.css"
+  },
+  {
+    "revision": "43d388e1d9b7fd4fda00",
+    "url": "/kit45/static/js/2.424ad9da.chunk.js"
+  },
+  {
+    "revision": "26d6bdea9cce883bf0a2",
+    "url": "/kit45/static/js/main.7e619bf7.chunk.js"
+  },
+  {
+    "revision": "a86921354e6d4e4e3379",
+    "url": "/kit45/static/js/runtime-main.abb76ae2.js"
+  }
+]);
